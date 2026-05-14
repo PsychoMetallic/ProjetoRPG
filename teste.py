a508798c8904ae57c81a51c43a3a07d5088cd29a
Guh.e <gustavo.marggrander.lopes@gmail.com>
2026-05-14 20:33:51 -0300

    append

--- teste.py
+++ teste.py
@@ -1,2 +1,24 @@
-import time
 
+historico = []
+saldo = 0
+
+while True:
+
+    valor = int(input("Insira um valor: "))
+
+    saldo += valor
+
+    registro = "Você depositou: " + str(saldo)
+
+    historico.append(registro) #APPEND = ANEXAR
+
+    registro2 = "Depositaste: " + str(saldo)
+
+    historico.append(registro2) #APPEND = ANEXAR
+
+    break
+
+# print(historico)
+
+for transacoes in historico:
+    print(">> " + registro)
